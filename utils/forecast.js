7eab646fbfac4c0854b3132e22e724b59a4c4aa9
mohammed2571994 <mohammed2571994@gmail.com>
2019-09-25 08:45:40 +0300

    Using destructuring and Property Shorthand

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/8dbb4ec24d5c0fe78a251ab7d2af9e61/' + latitude + ',' + longitude
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url: url, json: true }, (error, {body}) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
