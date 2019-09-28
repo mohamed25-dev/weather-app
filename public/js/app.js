e9d5d00bb0039a10da6f5a6ae43f679f90cfe60c
mohammed2571994 <mohammed2571994@gmail.com>
2019-09-28 21:32:32 +0300

    using fetch method to get the resquest from the endpoint

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,27 @@
-console.log('Hello There')
+const weatherForm = document.querySelector('form');
+const search      = document.querySelector('input');
+
+const message_1   = document.querySelector('#message-1');
+const message_2   = document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', (e)=> {
+    e.preventDefault();
+
+    message_1.textContent = 'Loading ..';
+    message_2.textContent = ' ';
+
+    const location = search.value;
+
+    fetch('http://localhost:3000/weather?search='+location).then((res)=> {
+        res.json().then((data)=> {
+            message_1.textContent = ' ';
+            if(data.error) {
+                console.log("error happened " + data.error);
+                message_2.textContent = data.error;
+            } else {
+                console.log(data);
+                message_2.textContent = data.forecast;
+            }
+        });
+    });
+});
