d037522955249da01b414b7fc67e116810a78be0
mohammed2571994 <mohammed2571994@gmail.com>
2019-09-28 21:33:52 +0300

    formating the code

--- utils/geocode.js
+++ utils/geocode.js
@@ -3,7 +3,7 @@ const request = require('request')
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoibW9oYW1tZWQyNTcxOTk0IiwiYSI6ImNrMHhjbHlncjAydDYzY3A4bmk0c3NxMTkifQ.iekf3_ErYq3KGehsNEzffQ&limit=1'
 
-    request({ url: url, json: true }, (error, {body}) => {
+    request({ url: url, json: true }, (error, {body}= {}) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
         } else if (body.features.length === 0) {
